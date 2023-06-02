a1d4a7a380a19ed4969901934ed2614a96ac7323
phucthien432002 <103552445+phucthien432002@users.noreply.github.com>
2023-06-02 21:14:13 +0700

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,100 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate: '%s - FinalProject',
+    title: 'FinalProject',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+        integrity: 'sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==',
+        crossorigin: 'anonymous',
+        referrerpolicy: 'no-referrer'
+      },
+      {rel:'shortcut icon' ,type:'image/x-icon' ,href:'../static/images/logo/iFi-logo-big.ico'},
+      {
+				rel: "stylesheet",
+				href: "https://fonts.googleapis.com/css?family=Poppins:300,500,700"
+			}
+    ],
+    script: [
+      {
+        src: 'https://kit.fontawesome.com/325c78919c.js',
+        crossorigin: 'anonymous'
+      },
+      { src: "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.10.4/gsap.min.js" },
+			{
+				src: "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.10.4/ScrollTrigger.min.js"
+			}
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    {src:"~/plugins/aos", ssr: false}
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: false,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    vendor: ["aos"] ,
+    extend(config, ctx){
+      if (ctx.isDev && ctx.isClient){
+        config.module.rules.push({
+          enforce: "pre",
+          test: /\.(js|vue|ts)$/,
+          loader: "eslint-loader",
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+ 
+}
